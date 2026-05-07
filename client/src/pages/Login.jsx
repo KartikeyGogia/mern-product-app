52842d2c41025fbf47a579740152c72d1b22dfb0
Kartikey Gogia <Kartikeygogia5@gmail.com>
2026-05-07 16:08:55 +0530

    Fixed login API URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -29,13 +29,15 @@ const handleLogout = () => {
 
     const handleLogin = async () => {
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ email, password })
-    });
+    const API_URL = import.meta.env.VITE_API_URL;
+
+const res = await fetch(`${API_URL}/api/auth/login`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ email, password })
+});
 
     const data = await res.json();
 
